5e36705f3c5b10615c97924fb287f81a731edea2
Jason Ash <jasonwadeash@gmail.com>
2014-10-24 19:24:53 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Jason Ash on 10/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Jason Ash on 10/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
